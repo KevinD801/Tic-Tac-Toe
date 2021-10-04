9527441a1a4824bc18bc567567a9835da9f0be6a
Kevin Do <kevin.do2082@gmail.com>
2021-10-04 16:58:07 -0500

    Updates

---
+++ Tic-Tac-Toe/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class Game
+    {
+        private bool _gameOver;
+        private Board _gameBoard;
+
+        /// <summary>
+        /// Begins the game
+        /// </summary>
+        public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                Draw();
+                Update();
+            }
+
+            End();
+        }
+
+        /// <summary>
+        /// Called when the game starts
+        /// </summary>
+        private void Start()
+        {
+            _gameBoard = new Board();
+            _gameBoard.Start();
+        }
+
+        /// <summary>
+        /// Called every time the game loops 
+        /// </summary>
+        private void Update()
+        {
+            _gameBoard.Update();
+        }
+
+        /// <summary>
+        /// Updates the display to reflect and changes made while running
+        /// </summary>
+        private void Draw()
+        {
+            Console.Clear();
+            _gameBoard.Draw();
+        }
+
+        /// <summary>
+        /// Called when the game ends
+        /// </summary>
+        private void End()
+        {
+            _gameBoard.End();
+        }
+
+        public static int GetInput()
+        {
+            int choice = -1;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                choice = -1;
+            }
+            return choice;
+        }
+    }
+}
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -5,40 +5,9 @@ namespace Tic_Tac_Toe
     class Program
     {
         static void Main(string[] args)
-        {                                                 /*0 1 2*/
-            int[,] array2Dexample = new int[3, 3] { /*0*/{ 1, 2, 3 }, 
-                                                    /*1*/{ 4, 5, 6 }, 
-                                                    /*2*/{ 7, 8, 9 } };
-            int[,] array2Dexample2 = { { 1, 2, 3 }, 
-                                       { 4, 5, 6 }, 
-                                       { 7, 8, 9 }, 
-                                       { 10, 11, 12 } };
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.WriteLine(array2Dexample[i, j]*6);
-                }
-            }
-
-
-
-            void AddRows(int[,] arr)
-            {
-                for (int i = 0; i < arr.GetLength(0); i++)
-                {
-                    int num1 = 0;
-
-                    for (int j = 0; j < arr.GetLength(1); j++)
-                    {
-                        num1 += arr[i, j];
-                    }
-                    Console.WriteLine(num1);
-                }
-            }
-
-            AddRows(array2Dexample);
+        {
+            Game game = new Game();
+            game.Run();
         }
     }
 }
